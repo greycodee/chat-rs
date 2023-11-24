96f6af36ca2c7f3b7c938b5940e8f7d5c792e0b0
greycode <zhengminghui99@gmail.com>
2023-11-24 15:56:39 +0800

    :recycle: file struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-pub mod client;
-pub mod handler;
-
+mod core;
 use std::{sync::Arc, collections::HashMap};
-
-use crate::handler::handler::handler_connect;
-
-use client::tcp_client::ChannelData;
 use tokio::{
     net::TcpListener,
     sync::{broadcast, Mutex},
@@ -14,13 +8,13 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:1212").await.unwrap();
     // 定义广播
-    let (tx, _) = broadcast::channel::<ChannelData>(16);
+    let (tx, _) = broadcast::channel::<core::client::ChannelData>(16);
     let map = Arc::new(Mutex::new(HashMap::<String, i32>::new()));
     // let map_clone = Arc::clone(&map);
     let mut id: u32 = 0;
     loop {
         let (client_stream, _) = listener.accept().await.unwrap();
-        tokio::spawn(handler_connect(client_stream, tx.clone(), id,map.clone()));
+        tokio::spawn(core::handler::handler_connect(client_stream, tx.clone(), id,map.clone()));
         id += 1;
     }
 }
