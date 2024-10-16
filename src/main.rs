d58343e077a97ca0933f39e177df2238d8b6471b
greycode <zhengminghui99@gmail.com>
2024-10-16 10:49:53 +0800

    update use path

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
-mod core;
 use std::{sync::Arc, collections::HashMap};
 use tokio::{
     net::TcpListener,
     sync::{broadcast, Mutex},
 };
+use core::handler;
+use core::client;
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:1212").await.unwrap();
     // 定义广播
-    let (tx, _) = broadcast::channel::<core::client::ChannelData>(16);
+    let (tx, _) = broadcast::channel::<client::ChannelData>(16);
     let map = Arc::new(Mutex::new(HashMap::<String, i32>::new()));
-    // let map_clone = Arc::clone(&map);
     let mut id: u32 = 0;
     loop {
         let (client_stream, _) = listener.accept().await.unwrap();
-        tokio::spawn(core::handler::handler_connect(client_stream, tx.clone(), id,map.clone()));
+        tokio::spawn(handler::handler_connect(client_stream, tx.clone(), id,map.clone()));
         id += 1;
     }
 }
